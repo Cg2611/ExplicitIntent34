cd7102aa4dbe6ff3985104ec5a71463a8e194d2f
Chaithanya Ganesh <96193964+Cg2611@users.noreply.github.com>
2024-01-27 12:50:44 +0530

    With ImplicitIntent to Webpage

--- app/src/main/java/com/example/explicitintent34/MainActivity.java
+++ app/src/main/java/com/example/explicitintent34/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.explicitintent34;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
+    Button btn,btn2;
 
 
     @Override
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.button);
+        btn2=findViewById(R.id.button2);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,9 +26,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                webpageselect();
+
+            }
+        });
     }
     public void secondintent(){
         Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+
+    }
+
+    public void webpageselect(){
+        Uri webpage = Uri.parse("https://www.google.com");
+        Intent intent= new Intent(Intent.ACTION_VIEW,webpage);
         startActivity(intent);
 
     }
